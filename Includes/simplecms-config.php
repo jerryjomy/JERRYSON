b05994f413a421ae3981890a0eabd309184dc580
jerryjomy <jerrysononasaa@gmail.com>
2014-07-08 08:43:48 +0300

    selling products

---
+++ Includes/simplecms-config.php
@@ -0,0 +1,9 @@
+<?php
+    define('DB_NAME', 'simplecms640');
+    define('DB_USER', 'simplecmsuser461');
+    define('DB_PASSWORD', 'F-IZK2%bc%mk');
+    define('DB_HOST', 'localhost');
+
+    define('DEFAULT_ADMIN_USERNAME', 'admin');
+    define('DEFAULT_ADMIN_PASSWORD', 'jerryjomy');
+?>
